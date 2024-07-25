51e81cc2479223fa8aa9a6e0b4e5d7c06c5ed5c7
Ankur Kalson <ankurkalson28@gmail.com>
2024-07-24 23:10:06 -0400

    added missing key to map

--- app/dashboard/upload/page.tsx
+++ app/dashboard/upload/page.tsx
@@ -36,7 +36,7 @@ function UploadPage() {
                 ) : (
                     <>
                         <CircleArrowDown className='h-20 w-20 animate-bounce'/>
-                        <p>Drag 'n' drop some files here, or click to select files</p> 
+                        <p>Drag n drop some files here, or click to select files</p> 
                     </>
                 )}
             </div>
